57af7e910d6f6875c96fe29b7d67ff79213baa23
MariaSchillstrom <77marsch@gafe.molndal.se>
2024-11-27 22:05:25 +0100

    feat: Skapade en ny klass för HandlerMenuChoice och ShowMenu refactor; flyttat databaslogik till egen klass, i detta fallet från Main

---
+++ HenriksHobbyLager/Services/MenuHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HenriksHobbyLager.Services
+{
+    internal class MenuHandler // flytta HandlerMenuChoice och ShowMenu till denna klass
+    {
+    }
+}
