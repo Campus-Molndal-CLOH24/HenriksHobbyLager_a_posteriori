9e1bbc323e1399edf6fe85cf6014643392ddc0ae
maria schillstrom <mariacschillstrom@gmail.com>
2024-12-02 15:01:16 +0100

    feat: new class

--- HenriksHobbyLager/Program.cs
+++ HenriksHobbyLager/Program.cs
@@ -1,7 +1,8 @@
 ﻿
 // Denna anropar HenriksHobbyLager.ProgramManager.HenriksHobbyLager.ProgramManagement.HenriksHobbyLagerProgramManager.Run() som är en metod som innehåller allt som ska köras i programmet.
 //För att hålla koden ren, skapade jag en egen klass för programlogiken. 
-using HenriksHobbyLager.ProgramManager.HenriksHobbyLager.ProgramManagement;
+using HenriksHobbyLager.ProgramManagement;
+
 
 internal class Program
 {
